b086373c8e699d82272af5368a9e994ce214375b
Allyson Dekovich <adekovic@vols.utk.edu>
2020-07-28 10:42:11 -0400

    alien_colors_2
    I intentionally made an error so I can fix it via terminal

---
+++ alien_colors_2.py
@@ -0,0 +1,14 @@
+alien_color = 'red'
+
+if alien_color = 'green':
+    print("You have just earned 5 points!")
+else: 
+    print("You have just earned 10 points!") #this statement prints 
+
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You have just earned 5 points!") #this statement prints
+else: 
+    print("You have just earned 10 points!"
